8ad1d7dadfa309e6dfece85548ce5abeef393b9e
haudppc07638 <danhphuchau3110@gmail.com>
2024-03-16 18:48:05 +0700

    giao dien ASM GD 1

---
+++ admin/categories/editcate.php
@@ -0,0 +1,13 @@
+<div class="container-fluid">
+    <div class="card-header">
+        <h2>Edit Category</h2>
+    </div>
+    <form method="post">
+        <div class="form-floating mb-3 mt-3">
+            <input type="text" class="form-control" name="cateName"
+                value="Đồng hồ nam">
+
+        </div>
+        <button name="editcate" class="btn btn-primary">Save</button>
+    </form>
+</div>
---
+++ admin/categories/list.php
@@ -0,0 +1,48 @@
+<div class="col-lg-12 grid-margin stretch-card">
+    <div class="card">
+        <div class="card-body">
+            <h4 class="card-title">List categories</h4>
+            <a href="?act=addcate"><button type="button" class="btn btn-outline-primary btn-fw">Add
+                    Category</button></a>
+            </p>
+            <table class="table table-bordered">
+                <thead>
+                    <tr>
+                        <th> ID </th>
+                        <th> Name </th>
+
+                        <th>Action</th>
+
+
+                    </tr>
+                </thead>
+                <tbody>
+                <tbody>
+
+                    <?php
+                    $dblist = new category();
+                    $rows = $dblist->getList();
+                    foreach ($rows as $row) { ?>
+                        <tr>
+                            <td>
+                                <? echo $row['categoryID'] ?>
+                            </td>
+                            <td>
+                                <? echo $row['categoryName'] ?>
+                            </td>
+
+                            <td styleact="text-align: center;">
+                                <a href="#"> <button type="button"
+                                        class="btn btn-danger">Delete</button></a> <br> <br>
+                                <a href="?act=editcate"><button type="button"
+                                        class="btn btn-success">Edit</button></a>
+                            </td>
+                        </tr>
+
+
+                    <?php } ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
